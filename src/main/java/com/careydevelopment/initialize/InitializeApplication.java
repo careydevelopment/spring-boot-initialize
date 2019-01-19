f05ffd6fe60068089a83442e99978a6818e5c97f
Brian Carey <mrbrianmcarey@gmail.com>
2019-01-19 06:44:31 -0500

    initialize

---
+++ src/main/java/com/careydevelopment/initialize/InitializeApplication.java
@@ -0,0 +1,12 @@
+package com.careydevelopment.initialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializeApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run(InitializeApplication.class,args);
+	}
+}
